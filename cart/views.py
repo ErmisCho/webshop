febba23eb7178b2480eaa53fe31c8833d7252cae
ErmisCho <ermis.chorinopoulos@gmail.com>
2025-09-10 21:45:41 +0200

    feat: add variation for cart items

--- cart/views.py
+++ cart/views.py
@@ -13,30 +13,6 @@ from django.conf import settings
 # Create your views here.
 
 
-def send_welcome_email(to_email, username, product_name):
-    subject = "Welcome to Our Website"
-    message = f"Hi {username},\n\nThank you for registering on our site! Feel free to get back to us regarding {product_name}.\n\nBest regards,\nThe Team"
-    email_from = settings.DEFAULT_FROM_EMAIL
-    recipient_list = [to_email]
-
-    try:
-        send_mail(subject, message, email_from, recipient_list)
-        return True
-    except Exception as e:
-        print("Error sending email:", e)
-        return False
-
-
-def test_email(request, product_name):
-    success = send_welcome_email(
-        "example@gmail.com", "Example", product_name)
-    print("Email function executed.")
-    if success:
-        print("Email sent successfully!")
-        return HttpResponse("Email sent successfully!")
-    return HttpResponse("Failed to send email.")
-
-
 def _cart_id(request):
     cart = request.session.session_key
     if not cart:
@@ -65,7 +41,7 @@ def add_cart(request, product_id):
         color = request.POST.get('color')
         size = request.POST.get('size')
     # size = request.GET.get('size')
-    print(color + " " + size)
+    # print(color + " " + size)
 
     try:
         cart = Cart.objects.get(cart_id=_cart_id(request))
@@ -75,16 +51,42 @@ def add_cart(request, product_id):
         )
     cart.save()
 
-    try:
-        cart_item = CartItem.objects.get(product=product, cart=cart)
-        cart_item.quantity += 1
-        cart_item.save()
-    except CartItem.DoesNotExist:
-        cart_item = CartItem.objects.create(
+    is_cart_item_exists = CartItem.objects.filter(
+        product=product, cart=cart).exists()
+    if is_cart_item_exists:
+        cart_item = CartItem.objects.filter(
             product=product,
-            quantity=1,
             cart=cart,
         )
+        ex_var_list = []
+        id = []
+        for item in cart_item:
+            existing_variation = item.variations.all()
+            ex_var_list.append(list(existing_variation))
+            id.append(item.id)
+        print(ex_var_list)
+
+        if product_variation in ex_var_list:
+            index = ex_var_list.index(product_variation)
+            item_id = id[index]
+            item = CartItem.objects.get(product=product, id=item_id)
+            item.quantity += 1
+            item.save()
+
+        else:
+            item = CartItem.objects.create(
+                product=product, quantity=1, cart=cart)
+            if len(product_variation) > 0:
+                item.variations.clear()
+                item.variations.add(*product_variation)
+            # cart_item.quantity += 1
+            item.save()
+    else:
+        cart_item = CartItem.objects.create(
+            product=product, quantity=1, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            cart_item.variations.add(*product_variation)
         cart_item.save()
     # return HttpResponse(cart_item.product)
     # test_email(request, product.product_name)
@@ -92,22 +94,27 @@ def add_cart(request, product_id):
     return redirect('cart')
 
 
-def remove_cart(request, product_id):
+def remove_cart(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
-    if cart_item.quantity > 1:
-        cart_item.quantity -= 1
-        cart_item.save()
-    else:
-        cart_item.delete()
+    try:
+        cart_item = CartItem.objects.get(
+            product=product, cart=cart, id=cart_item_id)
+        if cart_item.quantity > 1:
+            cart_item.quantity -= 1
+            cart_item.save()
+        else:
+            cart_item.delete()
+    except:
+        pass
     return redirect('cart')
 
 
-def remove_cart_item(request, product_id):
+def remove_cart_item(request, product_id, cart_item_id):
     cart = Cart.objects.get(cart_id=_cart_id(request))
     product = get_object_or_404(Product, id=product_id)
-    cart_item = CartItem.objects.get(product=product, cart=cart)
+    cart_item = CartItem.objects.get(
+        product=product, cart=cart, id=cart_item_id)
     cart_item.delete()
     return redirect('cart')
 
@@ -133,3 +140,27 @@ def cart(request, total=0, quantity=0, cart_items=None):
         'grand_total': grand_total,
     }
     return render(request, 'store/cart.html', context)
+
+
+def send_welcome_email(to_email, username, product_name):
+    subject = "Welcome to Our Website"
+    message = f"Hi {username},\n\nThank you for registering on our site! Feel free to get back to us regarding {product_name}.\n\nBest regards,\nThe Team"
+    email_from = settings.DEFAULT_FROM_EMAIL
+    recipient_list = [to_email]
+
+    try:
+        send_mail(subject, message, email_from, recipient_list)
+        return True
+    except Exception as e:
+        print("Error sending email:", e)
+        return False
+
+
+def test_email(request, product_name):
+    success = send_welcome_email(
+        "example@gmail.com", "Example", product_name)
+    print("Email function executed.")
+    if success:
+        print("Email sent successfully!")
+        return HttpResponse("Email sent successfully!")
+    return HttpResponse("Failed to send email.")
