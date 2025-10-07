19c126421a9175663b3cb709b3593a690b572a2a
ErmisCho <ermis.chorinopoulos@gmail.com>
2025-10-07 15:43:07 +0200

    feat: impelement product gallery on a per product basis

--- store/models.py
+++ store/models.py
@@ -3,7 +3,6 @@ from django.urls import reverse
 from accounts.models import Account
 from category.models import Category
 from django.db.models import Avg, Count
-# Create your models here.
 
 
 class Product(models.Model):
@@ -82,3 +81,16 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(
+        Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'
